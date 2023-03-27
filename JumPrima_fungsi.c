142761d8f307fb56fdd4434a5af03e0cb3eab218
aulyasalsabiila <128215223+aulyasalsabiila@users.noreply.github.com>
2023-03-28 03:48:30 +0700

    Add files via upload

---
+++ JumPrima_fungsi.c
@@ -0,0 +1,41 @@
+/* Nama File    : JumPrima_fungsi.c */
+/* Deskripsi    : Menampilkan bilangan Prima yang dapat dibentuk dari bilangan integer N sembarang > 0 */
+/* Pembuat      : Aulya Salsabila Khairunnisa - 24060122140163 */
+/* Tanggal Pembuatan : 28 Maret  2023 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int isPrime(int n){
+    if(n <= 1){
+        return 0;
+    }
+    for(int i = 2; i < n; i++){
+        if(n % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+/* Kamus */
+    int sum = 0;
+    int N; /* Bilangan yang akan ditentukan prima atau bukan */
+    int i;
+
+/* Algoritma */
+    printf("N=");
+    scanf("%d",&N);
+    if(N <= 1){
+        printf("Tidak Ada Bilangan Prima");
+    }
+    for(i=2; i <= N; i++){
+        if(isPrime(i)){
+            sum = sum + i;
+        }
+    }
+    printf(", sehingga Jumlahannya Sn=%d",sum);
+    return 0;
+}
